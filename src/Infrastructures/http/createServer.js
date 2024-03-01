3c147994461c220ab7679bb3e8c46430c54ddc9d
XrerXrerX <dikihidayat/dh@gmail.com>
2024-03-01 18:42:05 +0700

    test cd

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -65,7 +65,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/',
     handler: () => ({
-      value: 'Hello world!',
+      value: 'Hello world!!',
     }),
   });
 
